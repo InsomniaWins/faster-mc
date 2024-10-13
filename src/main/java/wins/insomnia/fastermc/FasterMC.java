a3befa13039ac6de9741b9325bd66a89e7174bda
Insomnia_Wins <insomniawins.business@gmail.com>
2024-10-13 15:18:15 -0500

    - Initial Commit

---
+++ src/main/java/wins/insomnia/fastermc/FasterMC.java
@@ -0,0 +1,17 @@
+package wins.insomnia.fastermc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FasterMC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
